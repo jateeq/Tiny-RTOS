362db88d0745b14532a85f98a4d2c0b160201aeb
Jawad Ateeq <jawad_atiq@hotmail.com>
2011-11-12 14:35:47 -0500

    made constants work

--- kernelAPI.c
+++ kernelAPI.c
@@ -16,39 +16,32 @@ int k_send_message ( int dest_process_id, msg_envelope * msg_envelope )
         return ERROR_INVALID_PID;
         
     PCB *target_PCB;
-    int n = NUM_OF_IPROC;
+    //int n = NUM_OF_IPROC;
 
     int i;
     
-    for (i=0;i<n;i++)
-    //use dest_id to look up the target process PCB pointer
+    for (i=0;i<NUM_OF_IPROC;i++) //use dest_id to look up the target process PCB pointer
     {
         if (dest_process_id == pcb_pointer_tracker[i]->process_id)
             target_PCB = pcb_pointer_tracker[i];
     }
-    msg_queue *temp_msg_q;
+    
+	msg_queue *temp_msg_q;
     temp_msg_q= &target_PCB->msg_envelope_q;
-    //enqueue envelope onto the message queue of the target process
+    
+	//enqueue envelope onto the message queue of the target process
     msg_envelope->sender_pid = current_process->process_id;
     msg_envelope->receiver_pid = dest_process_id;
     msg_enqueue(msg_envelope, temp_msg_q);
 	fflush(stdout);
 	printf("check target pcb queue size: %i\n", target_PCB->msg_envelope_q.size); 
+	
 	if (target_PCB->msg_envelope_q.head==NULL) {
 		printf("send failed: msg_envelope_q is null\n");
 	}
-	fflush(stdout);
-    //get sender_id and dest_id and store them into envelope
-    /*
-    printf("Message received from sender # ");
-    printf("%i",current_process->process_id);
-    printf("to receiver # ");
-    printf("%i\n",dest_process_id);*/
-	//store the details of this send transaction on the send_trace_buffer
+	fflush(stdout);        
     
-    n=BLOCKED_ON_RECEIVE;
-    
-    if(target_PCB->process_state == n)
+    if(target_PCB->process_state == BLOCKED_ON_RECEIVE)
     {
 		target_PCB->process_state = READY;
 		rpq_enqueue(target_PCB);//enqueue blocked process to ready queue;
@@ -58,19 +51,10 @@ int k_send_message ( int dest_process_id, msg_envelope * msg_envelope )
 }
 
 msg_envelope * k_receive_message()
-{
-	if ( current_process->process_id ==5) 
-	printf("k_recieve_message: current process pid is %i", current_process->process_id);
-        while ( current_process->msg_envelope_q.size == 0)
-	{	
-		/*if (current_process->process_priority != 0) 
-		{
-	           current_process->process_state=BLOCKED_ON_RECEIVE;
-	           process_switch();
-		}
-		else //this is iprocess
-		{*/
-		return NULL; //}
+{	
+    while ( current_process->msg_envelope_q.size == 0)
+	{			
+		return NULL;
 	}
     msg_queue *temp_queue;
     temp_queue = &current_process->msg_envelope_q;
@@ -83,46 +67,40 @@ msg_envelope * k_receive_message()
     printf("%i\n",current_process->process_id);
 	printf("k_message_receive: returning message");
 	//store the details of this receive transaction on the receive_trace_buffer
-printf("queue dequue: msg env: %d\n", temp_envelope->msg_size);
-printf("msg env envelope sender: %i\n", temp_envelope->sender_pid);
-printf("msg env envelope dest: %i\n", temp_envelope->receiver_pid);
+	printf("queue dequue: msg env: %d\n", temp_envelope->msg_size);
+	printf("msg env envelope sender: %i\n", temp_envelope->sender_pid);
+	printf("msg env envelope dest: %i\n", temp_envelope->receiver_pid);
 	return temp_envelope;
 }
 
 int k_terminate() // GLOBAL VARIABLE FOR: child process id, file id
 {
     int retCode=0;
-    int i;
-    //int z = sizeof(childpid); //this shit returns size 8 somehow
+    int i;    
     for (i=0;i<2;i++)
     {
-	fflush(stdout);
-	printf("childpid: %i\n", childpid[i]);
-	fflush(stdout);
+		fflush(stdout);
+		printf("childpid: %i\n", childpid[i]);
+		fflush(stdout);
         kill(childpid[i],SIGINT);
     }
-    // terminate child process(es)
-    //int n;
-    //n = BUFFER_SIZE; STUPID C DOES NOT RECOGNIZE THIS SHIT FIX LATER ASSHOLES
+    // terminate child process(es)    
     int statusfn1,statusfn2,statusfid,status1,status2;
     status1 = munmap(in_mem_ptr, 256); 
     status2 = munmap(out_mem_ptr, 256);
     
-    if (status1==-1 || status2==-1)
-    // remove shared memory segment and do some standard error checks
+    if (status1==-1 || status2==-1) // remove shared memory segment and do some standard error checks
     {
 	fflush(stdout);
         printf("BAD UNMAP\n");
         retCode = ERROR_BAD_MEMORY_UNMAP;
     }
     
-    i=0;
-    for (i;i<2;i++)
+    for (i=0;i<2;i++)
     {
         fflush(stdout);
-	printf("fid: %i\n", fileid[i]);
-        statusfid = close(fileid[i]); //this shit is wrooooooooooong
-        // close the temporary mmap file
+		printf("fid: %i\n", fileid[i]);
+        statusfid = close(fileid[i]);        
         
         if (statusfid == -1)
         {
@@ -144,38 +122,23 @@ int k_terminate() // GLOBAL VARIABLE FOR: child process id, file id
     }
  
     printf("Deallocating data structures\n");   
-
-   printf("Free free envelope queue\n");
-     free(free_env_Q);
-
-    printf("Free blocked on resource queue\n");
-     free(blocked_on_resource_Q);
+	printf("Free free envelope queue\n");
+    free(free_env_Q);
     
-     printf("Free sorted time list\n");
+	printf("Free blocked on resource queue\n");
+    free(blocked_on_resource_Q);
+    
+    printf("Free sorted time list\n");
     free(sorted_timeout_lst);
     
     printf("Free RPQ\n");
-    free(rpq);
-   
-   // printf("Free in_mem_ptr and out_mem_ptr pointers\n");
-   // free(in_mem_ptr); //Freeing these two pointers results an error message; aren't they already freed by munmap? 
-   // free(out_mem_ptr);
+    free(rpq);     
     
-    printf("Free created PCBs (pcb_pointer_tracker\n");
-    i=0;
-    for (i; i < 3; i++) {//*******8 for full implementation!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    printf("Free created PCBs (pcb_pointer_tracker\n");    
+    for (i=0; i < NUM_TOTAL_PROC; i++) {//*******8 for full implementation!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         free(pcb_pointer_tracker[i]);
 	printf("%i\n",i);
-    }   
-
-   //printf("Free mmap_ptr\n");
-   //free(mmap_ptr); Isn't the content that this guy points to already have been freed by mumap?
-   
-   // free(input_filename);DID WE MALLOC THIS?
-   // free(output_filename);
-   
- //   printf("Free current process pointer\n");
-//    free(current_process); //Freed already?
+    }     
 
     kill(getpid(), SIGINT); 
     exit(0);	
@@ -184,13 +147,12 @@ int k_terminate() // GLOBAL VARIABLE FOR: child process id, file id
 
 int k_send_console_chars(msg_envelope *message_envelope)
 {
-	int retCode = 0;
-        int n=IPROC_CRT
+	int retCode = 0;    
 	fflush(stdout);
 	printf("Sending message from process P to CRT\n");  
 	if (message_envelope != NULL)
     {		
-		retCode = k_send_message(n, message_envelope);
+		retCode = k_send_message(IPROC_CRT, message_envelope);
 	}
 	else 
 		retCode = ERROR_INVALID_MID;
@@ -200,16 +162,14 @@ int k_send_console_chars(msg_envelope *message_envelope)
 
 int k_get_console_chars(msg_envelope *message_envelope )
 {
-    int retCode = 0;
-    int n=IPROC_KBD
+    int retCode = 0;    
     
     if (message_envelope != NULL)
     {
 		printf("queue dequue: msg env: %d\n", message_envelope->msg_size);
-		retCode = k_send_message(n, message_envelope);
+		retCode = k_send_message(IPROC_KBD, message_envelope);
 		fflush(stdout);
-		printf("get console chars was here\n");
-		//kb_iproc();
+		printf("get console chars was here\n");		
     }
 	else
 		retCode = ERROR_INVALID_MID;
@@ -281,10 +241,9 @@ int k_change_priority(int new_priority, int target_process_id)
         return ERROR_INVALID_PRIORITY;
     }
     else
-    {
-        int n=NUM_OF_USER_PROC;
+    {        
         int i;
-        for (i = 0;i < n;i++)
+        for (i = 0;i < NUM_OF_USER_PROC;i++)
         {
             if (target_process_id == pcb_pointer_tracker[i]->process_id)
             {
